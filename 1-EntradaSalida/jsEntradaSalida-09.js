cdc5315689f99b1fb8fd238d939a9b431ab40c6d
unknown <elian.ch.2001@gmail.com>
2021-07-09 18:03:52 -0300

    ingreso 2021

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -6,5 +6,26 @@ en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
 
-	alert("ok");
+	let sueldo;
+	let aumento;
+	let operacion1;
+	let operacion2;
+	let operacion3;
+	let resultado;
+
+	sueldo=document.getElementById("txtIdSueldo").value;
+	sueldo=parseInt(sueldo);
+
+	aumento=prompt("Ingrese el aumento:");
+
+	operacion1=sueldo*aumento;
+	operacion2=operacion1/100;
+	operacion3=sueldo+operacion2;
+	resultado=operacion3;
+
+	document.getElementById("txtIdResultado").value=resultado;
+
+	/*Pa averiguar el descuen o aumento de un valor hay que multiplicar el valor por el 
+	porcentaje y dividirlo por 100*/
+
 }
