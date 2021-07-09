8696e45025c42a8096e3bad7c81520c0ddeab4c3
unknown <elian.ch.2001@gmail.com>
2021-07-09 17:49:08 -0300

    ingreso 2021

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,21 +5,85 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	
+	let valor1;
+	let valor2;
+	let suma;
+	let resultado;
+
+	valor1=document.getElementById("txtIdNumeroUno").value;
+	valor1=parseInt(valor1);
+
+	valor2=document.getElementById("txtIdNumeroDos").value;
+	valor2=parseInt(valor2);
+
+	suma=valor1+valor2;
+	resultado=suma;
+
+	alert("El resultado de la suma es: "+resultado);
+
 }
 
 function restar()
 {
 	
+	let valor3;
+	let valor4;
+	let resta;
+	let resultado;
+
+	valor3=document.getElementById("txtIdNumeroUno").value;
+	valor3=parseInt(valor3);
+
+	valor4=document.getElementById("txtIdNumeroDos").value;
+	valor4=parseInt(valor4);
+
+	resta=valor3-valor4;
+	resultado=resta;
+
+	alert("El resultado de la resta es: "+resultado);
+
 }
 
 function multiplicar()
 { 
 	
+	let valor5;
+	let valor6;
+	let multiplicar;
+	let resultado;
+
+	valor5=document.getElementById("txtIdNumeroUno").value;
+	valor5=parseInt(valor5);
+
+	valor6=document.getElementById("txtIdNumeroDos").value;
+	valor6=parseInt(valor6);
+
+	multiplicar=valor5*valor6;
+	resultado=multiplicar;
+
+	alert("Elresultado de la multiplicacion es: "+resultado);
+
 }
 
 function dividir()
 {
 	
+	let valor7;
+	let valor8;
+	let dividir;
+	let resultado;
+
+	valor7=document.getElementById("txtIdNumeroUno").value;
+	valor7=parseInt(valor7);
+
+	valor8=document.getElementById("txtIdNumeroDos").value;
+	valor8=parseInt(valor8);
+
+	dividir=valor7/valor8;
+	resultado=dividir;
+
+	alert("El resultado de la divicion es: "+resultado);
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -5,5 +5,23 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	alert("ok");
+	
+	let valor1;
+	let valor2;
+	let dividir;
+	let resultado;
+
+	valor1=document.getElementById("txtIdNumeroDividendo").value;
+	valor1=parseInt(valor1);
+
+	valor2=document.getElementById("txtIdNumeroDivisor").value;
+	valor2=parseInt(valor2);
+
+	dividir=valor1%valor2;
+	resultado=dividir;
+
+	alert("El resto es: "+resultado);
+
+	//El signo 5 es para sacar el resto de la divicion.
+
 }
