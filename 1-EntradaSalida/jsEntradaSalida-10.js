d4279bc6cc3dcdba6e8416fe5497e3e7973b7c3c
unknown <elian.ch.2001@gmail.com>
2021-07-09 18:13:35 -0300

    ingreso 2021

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,27 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+
+	let importe;
+	let descuento;
+	let operacion1;
+	let operacion2;
+	let operacion3;
+	let resultado;
+
+	importe=document.getElementById("txtIdImporte").value;
+	importe=parseInt(importe);
+
+	descuento=prompt("Ingrese el descuento:");
+
+	operacion1=importe*descuento;
+	operacion2=operacion1/100;
+	operacion3=importe-operacion2;
+	resultado=operacion3;
+
+	document.getElementById("txtIdResultado").value=resultado;
+
+
+
+
 }
